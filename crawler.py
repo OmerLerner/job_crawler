ed7e2802d354f70b11b75724125aee99df3f3784
OmerLerner <omerler@post.bgu.ac.il>
2022-08-01 12:36:13 +0300

    Added feature to load from txt file

--- crawler.py
+++ crawler.py
@@ -1,47 +1,55 @@
+from modules import file_handler
+from os.path import exists
+
 from websites.indeed import indeed_Parser
 from websites.linkedin import linkedin_Parser
 
 
+def check_validity(job_title, location, days, keywords):
+    if len(job_title) == 0 or len(location) == 0 or days == 0 or len(keywords) == 0:
+        return False
+    return True
+
+
 def main():
-    job_title = "Student"
-    location = "Israel"
+    job_title = "student"
+    location = "israel"
     days = 1
-    keywords = ['computer science', 'python', 'javascript', 'c++', 'java', 'software engineer']
+    keywords = ['computer science']
 
-    choice = input("Write 0 to use values currently saved in crawler.py, or 1 to input custom values:")
-    if choice == "1":
-        job_title = input("Write the job title to search for: ")
-        location = input("Write the city/country to search for jobs: ")
-        days = input("Write a number - Check jobs posted in last X days: ")
-        if days.isnumeric():
-            days = int(days)
-        else:
-            count = 1
-            while not days.isnumeric():
-                if count == 3:
-                    print("Bye bye.")
-                    exit()
-                elif count == 2:
-                    days = input("Last chance, write a number this time. I believe in you: ")
-                    count += 1
-                else:
-                    days = input(
-                        "Alright, you had your fun. Write a number to scan the jobs posted in the last X days:")
-                    count += 1
-        print("\n")
-        print("Write down all of the keywords to scan for in the job description, and seperate each one by a comma.")
-        print("For example: Python,Full-Stack,Backend Engineer")
-        keywords_string = input("Write the keywords here: ")
-        keywords = keywords_string.split(',')
+    values = []
 
-    if len(job_title) > 0 and len(location) > 0:
-        linkedin_parser = linkedin_Parser(job_title, location, days, keywords)
-        indeed_parser = indeed_Parser(job_title, location, days, keywords)
+    file_exists = exists('parameters.txt')
 
-        linkedin_parser.extract_jobs()
-        indeed_parser.extract_jobs()
+    if file_exists:
+        choice = input("Write 0 to use values currently in parameters.txt, or 1 to input custom values (and save them "
+                       "in parameters.txt), or 2 to use values saved in crawler.py:")
+    else:
+        choice = input("Write 1 to input values, or 2 to use values saved in crawler.py:")
 
-        print("Job search complete!")
+    if (not file_exists) and choice == '0':
+        print("Can't load values from a file that doesn't exist. Try again and use 1 as the input value.")
+        exit(-1)
+    else:
+        values = file_handler.handle_input(choice)
+
+    if values and len(values) == 4:
+        job_title = values[0]
+        location = values[1]
+        days = values[2]
+        keywords = values[3]
+    if not check_validity(job_title, location, days, keywords):
+        print("One of the parameters: job_title, location, days, keywords is invalid.")
+        exit(-1)
+
+    print("All values are valid! Starting to find jobs.")
+    linkedin_parser = linkedin_Parser(job_title, location, days, keywords)
+    indeed_parser = indeed_Parser(job_title, location, days, keywords)
+
+    linkedin_parser.extract_jobs()
+    indeed_parser.extract_jobs()
+
+    print("Job search complete!")
 
 
 if __name__ == '__main__':
